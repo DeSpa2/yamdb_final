1ce20d5ce96905decc3bacb3d2d1f50e9af30d7a
Денис Рассказов <DenSpas2@yandex.ru>
2023-05-10 23:29:42 +0700

    tests

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -5,17 +5,8 @@ from django.core.mail import send_mail
 from django.db.models import Avg
 from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework import (
-    filters,
-    mixins,
-    status,
-    viewsets,
-)
-from rest_framework.decorators import (
-    action,
-    api_view,
-    permission_classes,
-)
+from rest_framework import filters, mixins, status, viewsets
+from rest_framework.decorators import action, api_view, permission_classes
 from rest_framework.filters import SearchFilter
 from rest_framework.pagination import LimitOffsetPagination
 from rest_framework.permissions import AllowAny, IsAuthenticated
@@ -57,6 +48,7 @@ class CreateDestroyListViewSet(
 
 @api_view(['POST'])
 @permission_classes((AllowAny,))
+
 def get_token(request):
     serializer = TokenSerializer(data=request.data)
     if serializer.is_valid():
