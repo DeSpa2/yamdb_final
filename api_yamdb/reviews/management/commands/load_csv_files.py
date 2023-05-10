2a9e94a9ede82b0d2f2d5914efb5f03fb0f9e32d
Денис Рассказов <DenSpas2@yandex.ru>
2023-05-10 23:55:35 +0700

    tests

--- api_yamdb/reviews/management/commands/load_csv_files.py
+++ api_yamdb/reviews/management/commands/load_csv_files.py
@@ -1,8 +1,6 @@
 import csv
 import os
 
-from django.core.management.base import BaseCommand
-from django.db.utils import IntegrityError
 from reviews.models import (
     Category,
     Comment,
@@ -12,6 +10,9 @@ from reviews.models import (
     TitleGenre,
     User
 )
+from django.core.management.base import BaseCommand
+from django.db.utils import IntegrityError
+
 from api_yamdb.settings import CSV_FILES_DIR
 
 FILE_NAMES_CLASSES = {
